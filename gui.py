26d8157632845596e1cd2f363d4d4262ce02947b
Soulmorr <danyaboichuk@gmail.com>
2024-03-27 21:42:35 +0200

    day17-added add function

--- gui.py
+++ gui.py
@@ -1,11 +1,28 @@
 from modules import functions
 import tkinter
 import PySimpleGUI as sg
+
+
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a todo")
+input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-do App", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("My To-do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
 
